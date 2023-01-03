c1d345596d156a1c936f873fe36b09310e0089c8
Entregable1 <carlarielita@gmail.com>
2023-01-02 22:09:48 -0400

    version

---
+++ src/components/CardUser.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// const CardUser = ({user}) => {
+//     return (
+//         <article>
+//             <img src={user?.picture.large} alt="" /> 
+
+//             <ul>
+//                 <li><span className='bold'>Name: </span> {user?.name.title} {user?.name.first} {user?.name.last} </li>
+//                 <li><span className='bold'>Email: </span> {user?.email} </li>
+//                 <li><span className='bold'>Location: </span>{`${user?.location.city}, ${user?.location.country}`}</li>
+//                 <li><span className='bold'>Gender: </span> {user?.gender} </li>
+//             </ul> 
+//         </article>
+//     );
+// };
+
+// export default CardUser;
